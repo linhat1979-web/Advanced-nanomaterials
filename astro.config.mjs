72b3a57939648aadcbb31fb72b6665645dc8712f
linhat1979-web <linhat1979@gmail.com>
2026-03-26 20:22:24 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // 換成你的網址：https://<你的GitHub帳號>.github.io
   site: 'https://linhat1979-web.github.io',
-  // 換成你的專案名稱，前後都要有斜線
-  base: '/Advanced-nanomaterials/',
+  base: '/Advanced-nanomaterials/', // 👈 必須完全一致
+  output: 'static', // 確保是靜態輸出
 });
